fe80a8f55b33025e3348b6997228ddc82976c534
Rafael Keidi Oishi <rafael.keidi.o@gmail.com>
2022-07-12 06:25:00 -0300

    Implementando interface gráfica ao projetinho 'chute_um_numero' com a biblioteca PySimpleGUI

--- chute_um_numero.py
+++ chute_um_numero.py
@@ -1,4 +1,55 @@
 from random import random
+from PySimpleGUI import PySimpleGUI as sg
+# Layout e funções
+def mostrarTelaInicial():
+  sg.theme('Reddit')
+  tela1=[
+    [sg.Text('Este programa é um jogo para você advinhar um número aleatório.')],
+    [sg.Text('Ele sorteia um número de 0 até um número máximo de sua escolha.')],
+    [sg.Text('Depois te guia até acertar o número.')],
+    [sg.Text('Digite um número máximo para ele sortear um número para você chutar depois.')],
+    [sg.Text('Número Máximo:'),sg.Input(key='numero')],
+    [sg.Button('Sortear')]
+  ]
+  return sg.Window("Tela Inicial",tela1, finalize=True)
+
+def mostrarTelaPrimeiroChute(numeroMaximo):
+  sg.theme('Reddit')
+  tela2=[
+    [sg.Text('Chute um numero entre 0 e ' + str(numeroMaximo))],
+    [sg.Text('Chute:'),sg.Input(key='numero')],
+    [sg.Button('Chutar Número')]
+  ]
+  return sg.Window("Guia",tela2, finalize=True)
+
+def mostrarTelaChuteFoiMenor(numeroMaximo):
+  sg.theme('Reddit')
+  tela3=[
+    [sg.Text('O seu chute foi menor que o número sortido')],
+    [sg.Text('Chute um numero entre 0 e ' + str(numeroMaximo))],
+    [sg.Text('Chute:'),sg.Input(key='numero')],
+    [sg.Button('Chutar Número')]
+  ]
+  return sg.Window("Guia",tela3, finalize=True)
+
+def mostrarTelaChuteFoiMaior(numeroMaximo):
+  sg.theme('Reddit')
+  tela4=[
+    [sg.Text('O seu chute foi maior que o número sortido')],
+    [sg.Text('Chute um numero entre 0 e ' + str(numeroMaximo))],
+    [sg.Text('Chute:'),sg.Input(key='numero')],
+    [sg.Button('Chutar Número')]
+  ]
+  return sg.Window("Guia",tela4, finalize=True)
+
+def mostrarTelaDeSucesso(numeroSortido):
+  sg.theme('Reddit')
+  tela5=[
+    [sg.Text('Você descobriu o número sortido.')],
+    [sg.Text('O número sortido foi o ' + str(numeroSortido))],
+    [sg.Button('Advinhar Outro Número'),sg.Button('Fechar')]
+  ]
+  return sg.Window("Guia",tela5, finalize=True)
 
 def pulaLinhaNoConsole():
   print("\r\n")
@@ -37,10 +88,56 @@ def continuaTentandoChutar(chute,numeroSortido):
       chute = pedeChuteNovamente()
     mostraAcertoDoChute(chute,numeroSortido)
 
-numeroMaximo = pegaNumeroMaximo()
-chute = pegaChuteInicial(numeroMaximo)
-numeroSortido = pegaNumeroSortido(numeroMaximo)
-
-mostraAcertoDoChute(chute,numeroSortido)
-
-continuaTentandoChutar(chute,numeroSortido)
+# Telas 
+janela1 = mostrarTelaInicial()
+janela2 = None
+janela3 = None
+janela4 = None
+janela5 = None
+numeroMaximo = 0
+numeroSortido = 0
+chute = 0
+# Loop
+while True:
+  window, events, values = sg.read_all_windows()
+  if (window == janela1 or window == janela2 or window == janela3 or window == janela4 or window == janela5 ) and (events == sg.WIN_CLOSED or events == "Fechar"):
+    break
+  elif window == janela1 and events == "Sortear":
+    numeroMaximo = int(values['numero'])
+    numeroSortido = pegaNumeroSortido(numeroMaximo)
+    janela1.hide()
+    janela2 = mostrarTelaPrimeiroChute(numeroMaximo)
+  elif window == janela2 and events == "Chutar Número":
+    chute = int(values['numero'])
+    if chute == numeroSortido:
+      janela2.hide()
+      janela5 = mostrarTelaDeSucesso(numeroSortido)
+    elif chute < numeroSortido:
+      janela2.hide()
+      janela3 = mostrarTelaChuteFoiMenor(numeroMaximo)
+    elif chute > numeroSortido:
+      janela2.hide()
+      janela4 = mostrarTelaChuteFoiMaior(numeroMaximo)
+  elif window == janela3 and events == "Chutar Número":
+    chute = int(values['numero'])
+    if chute == numeroSortido:
+      janela3.hide()
+      janela5 = mostrarTelaDeSucesso(numeroSortido)
+    elif chute < numeroSortido:
+      janela3 = mostrarTelaChuteFoiMenor(numeroMaximo)
+    elif chute > numeroSortido:
+      janela3.hide()
+      janela4 = mostrarTelaChuteFoiMaior(numeroMaximo)
+  elif window == janela4 and events == "Chutar Número":
+    chute = int(values['numero'])
+    if chute == numeroSortido:
+      janela4.hide()
+      janela5 = mostrarTelaDeSucesso(numeroSortido)
+    elif chute < numeroSortido:
+      janela4.hide()
+      janela3 = mostrarTelaChuteFoiMenor(numeroMaximo)
+    elif chute > numeroSortido:
+      janela4 = mostrarTelaChuteFoiMaior(numeroMaximo)
+  elif window == janela5 and events == "Advinhar Outro Número":
+    janela5.hide()
+    janela1 = mostrarTelaInicial()
